8f5d30c961a566c9e2f069bb34033ec5f8f2a9b1
Digby White <digbywhite@gmail.com>
2017-04-21 21:53:11 +0100

    Generated LibGDX Project
    Generated using the LibGDX Project Generator:
    http://libgdx.badlogicgames.com/download.html

---
+++ desktop/src/com/superfun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.superfun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.superfun.SuperFun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SuperFun(), config);
+	}
+}
