c519a91a3b1053dde620cb66f625c12241904b0c
Digby White <digbywhite@gmail.com>
2017-04-23 22:58:19 +0100

    Added testing of the Entity class
    Added testing into the SuperFun.java class:
     - An instance of Entity is created.
     - Its position is represented with a Texture drawn at the entity's coordinates.
     - The entity is moved around using the W, A, S and D keys.

--- core/src/com/superfun/SuperFun.java
+++ core/src/com/superfun/SuperFun.java
@@ -5,29 +5,54 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Input.Keys;
 
 public class SuperFun extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture ship_texture;
+	Entity ship_entity;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		ship_texture = new Texture("ship.png");
+		ship_entity = new Entity();
+		ship_entity.speed_change = 2;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		// Input
+		ship_entity.speed_target_x = 0;
+		ship_entity.speed_target_y = 0;
+		
+		if (Gdx.input.isKeyPressed(Keys.D)) {
+			ship_entity.speed_target_x += 16;
+		} if (Gdx.input.isKeyPressed(Keys.A)) {
+			ship_entity.speed_target_x -= 16;
+		} if (Gdx.input.isKeyPressed(Keys.W)) {
+			ship_entity.speed_target_y += 16;
+		} if (Gdx.input.isKeyPressed(Keys.S)) {
+			ship_entity.speed_target_y -= 16;
+		}
+		
+		
+		// Graphics
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(ship_texture, ship_entity.position_x, ship_entity.position_y);
 		batch.end();
+		
+		
+		// Physics
+		ship_entity.update();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		ship_texture.dispose();
 	}
 }
